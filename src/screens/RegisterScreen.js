f17c3a8478da6d660c8d023ba81b48155007b3f3
ReynoldVaz <54735127+ReynoldVaz@users.noreply.github.com>
2021-04-07 12:25:20 +0530

    Update RegisterScreen.js

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,37 +1,37 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
-import { Text } from 'react-native-paper'
-import Background from '../components/Background'
-import Logo from '../components/Logo'
-import Header from '../components/Header'
-import Button from '../components/Button'
-import TextInput from '../components/TextInput'
-import BackButton from '../components/BackButton'
-import { theme } from '../core/theme'
-import { emailValidator } from '../helpers/emailValidator'
-import { passwordValidator } from '../helpers/passwordValidator'
-import { nameValidator } from '../helpers/nameValidator'
+import React, {useState} from "react";
+import {View, StyleSheet, TouchableOpacity} from "react-native";
+import {Text} from "react-native-paper";
+import Background from "../components/Background";
+import Logo from "../components/Logo";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import TextInput from "../components/TextInput";
+import BackButton from "../components/BackButton";
+import {theme} from "../core/theme";
+import {emailValidator} from "../helpers/emailValidator";
+import {passwordValidator} from "../helpers/passwordValidator";
+import {nameValidator} from "../helpers/nameValidator";
 
-const RegisterScreen = ({ navigation }) => {
-  const [name, setName] = useState({ value: '', error: '' })
-  const [email, setEmail] = useState({ value: '', error: '' })
-  const [password, setPassword] = useState({ value: '', error: '' })
+const RegisterScreen = ({navigation}) => {
+  const [name, setName] = useState({value: "", error: ""});
+  const [email, setEmail] = useState({value: "", error: ""});
+  const [password, setPassword] = useState({value: "", error: ""});
 
   const onSignUpPressed = () => {
-    const nameError = nameValidator(name.value)
-    const emailError = emailValidator(email.value)
-    const passwordError = passwordValidator(password.value)
+    const nameError = nameValidator(name.value);
+    const emailError = emailValidator(email.value);
+    const passwordError = passwordValidator(password.value);
     if (emailError || passwordError || nameError) {
-      setName({ ...name, error: nameError })
-      setEmail({ ...email, error: emailError })
-      setPassword({ ...password, error: passwordError })
-      return
+      setName({...name, error: nameError});
+      setEmail({...email, error: emailError});
+      setPassword({...password, error: passwordError});
+      return;
     }
     navigation.reset({
       index: 0,
-      routes: [{ name: 'HomeScreen' }],
-    })
-  }
+      routes: [{name: "MyDrawer"}],
+    });
+  };
 
   return (
     <Background>
@@ -42,7 +42,7 @@ const RegisterScreen = ({ navigation }) => {
         label="Name"
         returnKeyType="next"
         value={name.value}
-        onChangeText={(text) => setName({ value: text, error: '' })}
+        onChangeText={(text) => setName({value: text, error: ""})}
         error={!!name.error}
         errorText={name.error}
       />
@@ -50,7 +50,7 @@ const RegisterScreen = ({ navigation }) => {
         label="Email"
         returnKeyType="next"
         value={email.value}
-        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        onChangeText={(text) => setEmail({value: text, error: ""})}
         error={!!email.error}
         errorText={email.error}
         autoCapitalize="none"
@@ -62,7 +62,7 @@ const RegisterScreen = ({ navigation }) => {
         label="Password"
         returnKeyType="done"
         value={password.value}
-        onChangeText={(text) => setPassword({ value: text, error: '' })}
+        onChangeText={(text) => setPassword({value: text, error: ""})}
         error={!!password.error}
         errorText={password.error}
         secureTextEntry
@@ -70,29 +70,28 @@ const RegisterScreen = ({ navigation }) => {
       <Button
         mode="contained"
         onPress={onSignUpPressed}
-        style={{ marginTop: 24 }}
-      >
+        style={{marginTop: 24}}>
         Sign Up
       </Button>
       <View style={styles.row}>
         <Text>Already have an account? </Text>
-        <TouchableOpacity onPress={() => navigation.replace('LoginScreen')}>
+        <TouchableOpacity onPress={() => navigation.replace("LoginScreen")}>
           <Text style={styles.link}>Login</Text>
         </TouchableOpacity>
       </View>
     </Background>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   row: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 4,
   },
   link: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: theme.colors.primary,
   },
-})
+});
 
-export default RegisterScreen
+export default RegisterScreen;
